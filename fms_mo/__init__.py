b6d878e03f88a55e25ea0b4420588b6a62fbdeae
Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>
2025-02-13 18:28:33 -0800

    fmt
    Signed-off-by: Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>

--- fms_mo/__init__.py
+++ fms_mo/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""FMS Model Optimizer init. Import most commonly used functions and classes here."""
+"""FMS Model Optimizer init. Imports for most commonly used functions and classes are here."""
 
 # Standard
 from importlib.metadata import PackageNotFoundError, version
