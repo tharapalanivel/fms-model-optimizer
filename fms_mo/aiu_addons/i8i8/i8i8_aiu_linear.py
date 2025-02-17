fb870bdaa51e49069ed8883c343a51077efd586f
Andrea Fasoli <andrea.fasoli@ibm.com>
2025-02-17 18:43:42 +0000

    Support for int8 smoothquant with fms0.0.8
    Signed-off-by: Andrea Fasoli <andrea.fasoli@ibm.com>

--- fms_mo/aiu_addons/i8i8/i8i8_aiu_linear.py
+++ fms_mo/aiu_addons/i8i8/i8i8_aiu_linear.py
@@ -15,6 +15,7 @@
 
 # Standard
 from dataclasses import dataclass
+from functools import partial
 from typing import Any, Mapping, Optional
 
 # Third Party
@@ -201,7 +202,7 @@ def get_int8_aiu_linear(
     out_features: int,
     bias: bool,
     linear_config: Optional[Mapping[str, Any]] = None,
-    use_smoothquant: bool = True,
+    use_smoothquant: bool = False,
 ) -> torch.nn.Module:
     """Retrieve a W8A8 Linear module"""
 
@@ -281,4 +282,8 @@ def shard_int8_aiu_linear(
 
 
 register_linear_type_to_module_map("int8_aiu", get_int8_aiu_linear)
+register_linear_type_to_module_map(
+    "int8_smoothquant_aiu",
+    partial(get_int8_aiu_linear, use_smoothquant=True),
+)
 register_linear_type_to_sharding_map("int8_aiu", shard_int8_aiu_linear)
