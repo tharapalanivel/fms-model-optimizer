a59010ee8bf609eb034463cc12eef5e2878ccffb
cliu-us <cliu@us.ibm.com>
2025-03-27 18:55:02 +0000

    include mx Linear class directly for convenience
    Signed-off-by: cliu-us <cliu@us.ibm.com>

--- examples/MX/ffn_tmp.py
+++ examples/MX/ffn_tmp.py
@@ -1,10 +1,101 @@
 # Third Party
-# import argparse
+
+# from mx import Linear as Linear_mx  # Need to amend mx's Linear class
 # Third Party
-from mx import Linear as Linear_mx  # Need to amend mx's Linear class
+from mx.elemwise_ops import quantize_elemwise_op
+from mx.linear import linear
+from mx.specs import apply_mx_specs, mx_assert_test
 import numpy as np
 import torch
 import torch.nn.functional as F
+
+
+class LinearMX(torch.nn.Linear):  # amend init and repr
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        bias=True,
+        mx_specs=None,
+        name=None,
+        **kwargs,  # [CL] current qmodel_prep will pass lots of stuff in
+    ):
+        mx_assert_test(mx_specs)
+        self.mx_none = mx_specs is None
+
+        self.name = name
+        self.prequantized_weights = False
+        self.mx_specs = apply_mx_specs(mx_specs)
+        super().__init__(
+            in_features, out_features, bias, device=kwargs.get("device", "cuda")
+        )  # [CL] would like to pass device to nn.Linear
+
+    def apply_mx_specs(self, mx_specs):
+        mx_assert_test(mx_specs)
+        self.mx_none = mx_specs is None
+        self.mx_specs = apply_mx_specs(mx_specs)
+
+    def append_name(self, postfix):
+        self.name += postfix
+
+    def prequantize_weights(self):
+        # Can't prequantize if not using bfloat weights
+        if self.mx_none:
+            return
+
+        assert (
+            self.mx_specs["round"] == "even"
+        ), "Bfloat round should be 'even' for prequantizing weights."
+        assert (
+            torch.cuda.is_bf16_supported()
+        ), "Current device does not support bfloat16"
+        assert self.mx_specs[
+            "bfloat_subnorms"
+        ], "Bfloat_subnorms should be set to True for prequantizing weights."
+        assert (
+            self.mx_specs["bfloat"] == 16
+        ), "Only Bfloat16 is supported for prequantizing weights."
+
+        with torch.no_grad():
+            self.weight.data = quantize_elemwise_op(
+                self.weight.data,
+                mx_specs=self.mx_specs,
+                round=self.mx_specs["round_weight"],
+            ).to(torch.bfloat16)
+
+            if self.bias is not None:
+                self.bias.data = quantize_elemwise_op(
+                    self.bias.data,
+                    mx_specs=self.mx_specs,
+                    round=self.mx_specs["round_weight"],
+                ).to(torch.bfloat16)
+
+        self.prequantized_weights = True
+
+    def forward(self, inputs):
+        if self.mx_none:
+            return super().forward(inputs)
+
+        if self.prequantized_weights:
+            assert not self.training, "Cannot use prequantized weights when training!"
+
+        return linear(
+            input=inputs,
+            weight=self.weight,
+            bias=self.bias,
+            mx_specs=self.mx_specs,
+            prequantized_weights=self.prequantized_weights,
+            name=self.name,
+        )
+
+    def extra_repr(self) -> str:
+        """[CL] to make it clear it's MX Linear"""
+        repr_str = (
+            f"in={self.in_features},out={self.out_features},"
+            f"w_fmt={self.mx_specs['w_elem_format']},a_fmt={self.mx_specs['a_elem_format']}"
+            f"blk_size={self.mx_specs['block_size']}"
+        )
+        return repr_str
 
 
 class ResidualMLP(torch.nn.Module):
@@ -81,7 +172,7 @@ if __name__ == "__main__":
     #       it's just an explicit test for now
     qcfg["mx_specs"] = mx_specs
     mlp = ResidualMLP(128)  # fresh model
-    MXLinear = partial(Linear_mx, mx_specs=qcfg["mx_specs"])
+    MXLinear = partial(LinearMX, mx_specs=qcfg["mx_specs"])
     qcfg["mapping"] = {
         torch.nn.Linear: {
             "from": torch.nn.Linear,
