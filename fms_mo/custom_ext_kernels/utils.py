a6eed1da381a68d76a5c27c7797de78ac1cfae46
cliu-us <cliu@us.ibm.com>
2025-06-04 13:33:31 +0000

    fix typo
    Signed-off-by: cliu-us <cliu@us.ibm.com>

--- fms_mo/custom_ext_kernels/utils.py
+++ fms_mo/custom_ext_kernels/utils.py
@@ -613,7 +613,7 @@ def exllama_ops_load_and_reg(qcfg=None, run_unit_test=False):
                 (x.shape[0], q4_width), dtype=torch.float16, device=x.device
             )
 
-            gptqmodel.exllamav2_kernels.gemm_half_q_half(
+            gptqmodel_exllamav2_kernels.gemm_half_q_half(
                 x, q_handle, output, force_cuda
             )
             return output.view(outshape)
